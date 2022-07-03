a428890b42ac88345c709839536f20402cc7fe15
Dinela <59210189+dinelaganovic@users.noreply.github.com>
2022-07-03 12:26:49 +0200

    Add files via upload

--- TicTacToe_OpenGL/main.cpp
+++ TicTacToe_OpenGL/main.cpp
@@ -5,7 +5,7 @@
 #include <math.h>    // Math library (Higher math functions )
 #include <string.h>
 // Svetla
-GLfloat LightAmbient[] = {0.0, 0.0, 1.0};
+GLfloat LightAmbient[] = {4.0, 0.0, 0.0};
 GLfloat LightDiffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
 GLfloat LightPosition[] = {0.0, 1.0, 0.0, 1.0};
 GLfloat LightSpecular[] = {1.0, 1.0, 1.0, 1.0};
@@ -13,48 +13,70 @@ GLfloat mat_diffuse[]={0.1, 0.1, 0.3, 1.0};
 GLfloat mat_ambient[]={0.1, 0.2, 0.1, 1.0};
 GLfloat mat_shininess = 5.0;
 int abc = 0;
-// mouse variables: Win = windows size, mouse = mouse position
+GLuint texture;
+GLuint texture1;
+GLuint texture2;
+GLuint texture3;
+
+//Funkcija za iscrtavanje texture
+GLuint LoadTexture(const char* filename, int width, int height){
+       GLuint texture;
+       unsigned char* data;
+       FILE* file;
+       file=fopen(filename, "rb");
+       if(file==NULL)return 0;
+       data=(unsigned char*)malloc(width * height * 3);
+       fread(data,width * height * 3,1,file);
+       fclose(file);
+       glGenTextures(1,&texture);
+       glBindTexture(GL_TEXTURE_2D,texture);
+       glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE);
+       glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+       glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+       glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+       glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+       glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+       free(data);
+       return texture;
+}
+
+
+// Win = windows size, mouse = mouse position
 int mouse_x, mouse_y, Win_x, Win_y, object_select;
-// Use to spin X's and O's
 int spin, spinboxes;
-// Win = 1 player wins, -1 computer wins, 2 tie.
-// player or computer; 1 = X, -1 = O
-// start_game indicates that game is in play.
+// Win = 1 igrac pobedio, -1 computer pobedio, 2 nereseno.
+// irač or computer; 1 = X, -1 = O
 int player, computer, win, start_game;
-// alignment of boxes in which one can win
-// We have 8 possibilities, 3 across, 3 down and 2 diagonally
+// Imamo 8 mogućnostu, 3 kolone, 3 reda i 2 dijagonale
 //
 // 0 | 1 | 2
 // 3 | 4 | 5
 // 6 | 7 | 8
 //
-// row, column, diagonal information
+// row, column, diagonal informacije
 static int box[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-// Storage for our game board
-// 1 = X's, -1 = O's, 0 = open space
+// nasa tabla matrica
+// 1 = X, -1 = O, 0 = prazno mesto
 int box_map[9];
-// center x,y location for each box
-int object_map[9][2] = {{-6, 6}, {0, 6}, {6, 6}, {-6, 0}, {0, 0}, {6, 0}, {-6, -6}, {0, -6}, {6, -6}}; // quadric pointer for build our X
+//moguće lokacije za nase vrednosti
+int object_map[9][2] = {{-6, 6}, {0, 6}, {6, 6}, {-6, 0}, {0, 0}, {6, 0}, {-6, -6}, {0, -6}, {6, -6}}; // pokazivac za X
 GLUquadricObj *Cylinder;
-// Begin game routine
+// pocetak igrea
 void init_game(void)
 {
     int i;
-    // Clear map for new game
     for (i = 0; i < 9; i++)
     {
         box_map[i] = 0;
     }
-    // Set 0 for no winner
     win = 0;
     start_game = 1;
 }
-// Check for three in a row/column/diagonal
-// returns 1 if there is a winner
+
 int check_move(void)
 {
     int i, t = 0;
-    //Check for three in a row
+   // proveravanje 3 polja
     for (i = 0; i < 8; i++)
     {
         t = box_map[box[i][0]] + box_map[box[i][1]] + box_map[box[i][2]];
@@ -65,7 +87,7 @@ int check_move(void)
         }
     }
     t = 0;
-    // check for tie
+    // provera kad je nerešeno
     for (i = 0; i < 8; i++)
     {
         t = t + abs(box_map[box[i][0]]) + abs(box_map[box[i][1]]) + abs(box_map[box[i][2]]);
@@ -74,7 +96,7 @@ int check_move(void)
         return (2);
     return (0);
 }
-// Do we need to block other player?
+// Provera da li treba da blokiramo drugog igraca
 int blocking_win(void)
 {
     int i, t;
@@ -83,7 +105,7 @@ int blocking_win(void)
         t = box_map[box[i][0]] + box_map[box[i][1]] + box_map[box[i][2]];
         if ((t == 2) || (t == -2))
         {
-            // Find empty
+            // Ako je prazno proveri
             if (box_map[box[i][0]] == 0)
                 box_map[box[i][0]] = computer;
             if (box_map[box[i][1]] == 0)
@@ -95,7 +117,7 @@ int blocking_win(void)
     }
     return (0);
 }
-// check for a free space in corner
+// provera slobodnog polja u čoskovima table
 int check_corner(void)
 {
     int i;
@@ -124,7 +146,7 @@ int check_corner(void)
         return (1);
     }
     return (0);
-} // Check for free space in row
+} //proveri slobodno mesto u redu
 int check_row(void)
 {
     if (box_map[4] == 0)
@@ -154,7 +176,7 @@ int check_row(void)
     }
     return (0);
 }
-// logic for computer's turn
+// potez kompjutera
 int computer_move()
 {
     if (blocking_win() == 1)
@@ -165,60 +187,56 @@ int computer_move()
         return (1);
     return (0);
 }
-// I use this to put text on the screen
+//ispis teksta na ekranu
 void Sprint(int x, int y, char *st)
 {
     int l, i;
-    l = strlen(st);         // see how many characters are in text string.
-    glRasterPos2i( x, y); // location to start printing text
-    for (i = 0; i < l; i++) // loop until i is greater then l
+    l = strlen(st);
+    glRasterPos2i( x, y); // lokacija za ispis teksta
+    for (i = 0; i < l; i++)
     {
 
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, st[i]); // Print a character on the screen
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, st[i]);
     }
 }
 void SPrint(int x, int y, char *st)
 {
     int l, i;
-    l = strlen(st);         // see how many characters are in text string.
-    glRasterPos2i( x, y); // location to start printing text
-    for (i = 0; i < l; i++) // loop until i is greater then l
+    l = strlen(st);
+    glRasterPos2i( x, y);
+    for (i = 0; i < l; i++)
     {
 
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, st[i]); // Print a character on the screen
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, st[i]);
     }
 }
-// This creates the spinning of the cube.
+// funkcija za rotaciju oblika - ako je preko 360 stepeni rotacija presla kreci od nule
 static void TimeEvent(int te)
 {
-    spin++; // increase cube rotation by 1
+    spin++; //
     if (spin > 360)
-        spin = 180;                 // if over 360 degress, start back at zero.
-    glutPostRedisplay();            // Update screen with new rotation data
-    glutTimerFunc(8, TimeEvent, 1); // Reset our timmer.
+        spin = 180;
+    glutPostRedisplay();
+    glutTimerFunc(8, TimeEvent, 1); // resetovanje tajmera
 }
-// Setup our Opengl world, called once at startup.
+// pocetni ekran
 void init(void)
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0); // When screen cleared, use black.
-    glShadeModel(GL_SMOOTH);          // How the object color will be rendered smooth or flat
-    glEnable(GL_DEPTH_TEST);          // Check depth when rendering
-    // Lighting is added to scene
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    // dodavanje svetla
     glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);
     glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);
     glLightfv(GL_LIGHT1, GL_SPECULAR, LightSpecular);
     glLightfv(GL_LIGHT1, GL_POSITION, LightPosition);
-    //glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-    //glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-    //glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-    //glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
     glShadeModel(GL_SMOOTH);
     glEnable(GL_NORMALIZE);
-    glEnable(GL_LIGHTING); // Turn on lighting
-    glEnable(GL_LIGHT1);   // Turn on light 1
+    glEnable(GL_LIGHTING); // paljenje svetla
+    glEnable(GL_LIGHT1);   // paljenje prvog svetla
     start_game = 0;
     win = 0;
-    // Create a new quadric
+    // kreiranje objekta
     Cylinder = gluNewQuadric();
     gluQuadricDrawStyle(Cylinder, GLU_FILL);
     gluQuadricNormals(Cylinder, GLU_SMOOTH);
@@ -253,18 +271,30 @@ void Draw_X(int x, int y, int z, int a)
     glPopMatrix();
     glPopMatrix();
 }
-// Draw our world
+
 void display(void)
 {
-    if (abc == 3)
+    //////
+      if (abc == 3)
     {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_TEXTURE_2D);//Pokrenuti texture
+        glPushMatrix();
+        glBindTexture(GL_TEXTURE_2D,texture);
+        glBegin(GL_POLYGON);
+         glTexCoord2d(0.0, 16.0); glVertex2f(-10.5,14.0);//leva prva
+         glTexCoord2d(10.0, 11.0); glVertex2f(5.5,-0.5);
+         glTexCoord2d(-6.0, 77.0); glVertex2f(21.5,0.5);//desna 3 tacka
+         glTexCoord2d(22.0, 2.0); glVertex2f(8.5,11.5);//tacka 2
+        glEnd();
+        glPopMatrix();
+        glPushMatrix();
         glColor3f(0.0, 0.0, 0.0);
-        glMatrixMode(GL_PROJECTION);              // Tell opengl that we are doing project matrix work
-        glLoadIdentity();                         // Clear the matrix
-        glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 30.0); // Setup an Ortho view
-        glMatrixMode(GL_MODELVIEW);               // Tell opengl that we are doing model matrix work. (drawing)
-        glLoadIdentity();                         // Clear the model matrix
+        glMatrixMode(GL_PROJECTION);              // rad u matrici
+        glLoadIdentity();
+        glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 30.0); // Ortho izgled
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
         glDisable(GL_COLOR_MATERIAL);
         glDisable(GL_LIGHTING);
         glColor3f(0.9, 0.8, 0.7);
@@ -275,7 +305,7 @@ void display(void)
         Sprint(-7, 2, "Overview");
         glColor3f(0.1, 0.2, 0.3);
         SPrint(-7, 1, "Tic Tac Toe is a two-player game where the first player to");
-        SPrint(-7, 0,  "connect a line of pieces from one side or corner of the board to the other wins.");
+        SPrint(-7, 0,  "connect a line of pieces from one side or corner of the board.");
         glColor3f(0.9, 0.8, 0.7);
         Sprint(-7, -2, "Rules");
         glColor3f(0.1, 0.2, 0.3);
@@ -289,12 +319,23 @@ void display(void)
     }
     else if (abc == 0)
     {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the screen
-        glMatrixMode(GL_PROJECTION);                        // Tell opengl that we are doing project matrix work
-        glLoadIdentity();                                   // Clear the matrix
-        glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 30.0);           // Setup an Ortho view
-        glMatrixMode(GL_MODELVIEW);                         // Tell opengl that we are doing model matrix work. (drawing)
-        glLoadIdentity();                                   // Clear the model matrix
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_TEXTURE_2D);//Pokrenuti texture
+        glPushMatrix();
+        glBindTexture(GL_TEXTURE_2D,texture);
+        glBegin(GL_POLYGON);
+         glTexCoord2d(0.0, 16.0); glVertex2f(-10.5,14.0);//leva prva
+         glTexCoord2d(0.0, 1.0); glVertex2f(-0.5,-0.5);
+         glTexCoord2d(-6.0, 77.0); glVertex2f(21.5,0.5);//desna 3 tacka
+         glTexCoord2d(22.0, 2.0); glVertex2f(8.5,11.5);//tacka 2
+        glEnd();
+        glPopMatrix();
+        glPushMatrix();
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 30.0);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
         glDisable(GL_COLOR_MATERIAL);
         glDisable(GL_LIGHTING);
         glColor3f(0.9, 0.8, 0.7);
@@ -311,12 +352,12 @@ void display(void)
         int ix, iy;
         int i;
         int j;
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the screen
-        glMatrixMode(GL_PROJECTION);                        // Tell opengl that we are doing project matrix work
-        glLoadIdentity();                                   // Clear the matrix
-        glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 30.0);           // Setup an Ortho view
-        glMatrixMode(GL_MODELVIEW);                         // Tell opengl that we are doing model matrix work. (drawing)
-        glLoadIdentity();                                   // Clear the model matrix
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-9.0, 9.0, -9.0, 9.0, 0.0, 30.0);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
         glDisable(GL_COLOR_MATERIAL);
         glDisable(GL_LIGHTING);
         glColor3f(0.9, 0.8, 0.7);
@@ -326,7 +367,6 @@ void display(void)
             Sprint(-2, 8, "Computer Won");
         if (win == 2)
             Sprint(-2, 8, "Draw");
-        // Setup view, and print view state on screen
         glColor3f(0.0, 0.0, 1.0);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
@@ -395,21 +435,23 @@ void reshape(int w, int h)
 } // Read the keyboard
 void keyboard(unsigned char key, int x, int y)
 {
-    switch (key)
-    {
-    case 27:
-        exit(0); // exit program when [ESC] key presseed
-        break;
-    default:
-        break;
+    if (key == '1') {
+       exit(0);
     }
+    if (key == '2') {
+        glEnable(GL_LIGHT1);
+    }
+    if (key == '3') {
+        glDisable(GL_LIGHT1);
+    }
+    glutPostRedisplay();
 }
 void mouse(int button, int state, int x, int y)
 {
-    // We convert windows mouse coords to out openGL coords
+
     mouse_x = (18 * (float)((float)x / (float)Win_x)) / 6;
     mouse_y = (18 * (float)((float)y / (float)Win_y)) / 6;
-    // What square have they clicked in?
+    // pozicija klika u kom polju je kliknuto
     object_select = mouse_x + mouse_y * 3;
     if (start_game == 0)
     {
@@ -476,9 +518,11 @@ void menu(int choice)
         break;
     }
 }
+
 // Main program
 int main(int argc, char **argv)
 {
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(850, 600);
@@ -496,6 +540,11 @@ int main(int argc, char **argv)
     glutKeyboardFunc(keyboard);
     //glutMouseFunc(mouse);
     glutTimerFunc(50, TimeEvent, 1);
+//teksture
+    texture=LoadTexture("./textures/texture2.raw",256, 256);
+    texture1=LoadTexture("./textures/texture3.raw",256, 256);
+    texture2=LoadTexture("./textures/texture.raw",256, 256);
+    texture3=LoadTexture("./textures/texture1.raw",256, 256);
     glutMainLoop();
     return 0;
 }
